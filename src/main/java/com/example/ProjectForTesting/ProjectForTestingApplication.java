878331e0972a0ab7b599e8a9088300e22d48aeaf
ayazFromCentralAsia <wiliamraartur@gmail.com>
2024-08-30 21:22:48 +0600

    first commit

---
+++ src/main/java/com/example/ProjectForTesting/ProjectForTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectForTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectForTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjectForTesting/ProjectForTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectForTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectForTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
